a9ee5702409cb3662729ae49db7486329f5acda8
Dominic Anthony <101585288+MyselfDominic@users.noreply.github.com>
2022-05-30 15:03:40 +0200

    Delete ResumeUpload directory

--- ResumeUpload/src/main/java/MidTerm/Exam/repository/FileRepository.java
+++
@@ -1,8 +0,0 @@
-package MidTerm.Exam.repository;
-
-import MidTerm.Exam.model.FileModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<FileModel, Long> {
-
-}
